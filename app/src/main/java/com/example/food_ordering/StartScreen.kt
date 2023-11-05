d179912ca97c9700b17966db6f741804f5904401
thuandevnguyen <thuandnk203@gmail.com>
2023-11-05 23:52:41 +0700

    start app

---
+++ app/src/main/java/com/example/food_ordering/StartScreen.kt
@@ -0,0 +1,24 @@
+package com.example.food_ordering
+
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.food_ordering.databinding.ActivityStartScreenBinding
+
+
+class StartScreen : AppCompatActivity() {
+
+    private val binding: ActivityStartScreenBinding by lazy {
+        ActivityStartScreenBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.btnNextStart.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
